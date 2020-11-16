b6ac32b610a8dafd7ab068518968b7a44ea9f850
TankistPro <nakol.nayk@mail.ru>
2020-11-16 13:52:51 +0300

    add coordinate grid

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QPixmap
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 from ui import Ui_GraphsApp
 
 class App(QtWidgets.QMainWindow, Ui_GraphsApp):
@@ -25,25 +26,37 @@ class App(QtWidgets.QMainWindow, Ui_GraphsApp):
         self.pushButton.clicked.connect(self.drawGraph)
 
     def drawGraph(self):
-        #x = int(self.spinBox.text())
-        expression = self.lineEdit.text()
+        #Берм диапазон X
+        minX = int(self.minX.text())
+        maxX = int(self.maxX.text())
+        expression = self.lineEdit.text().replace(" ", "")
+        if("/0" in expression):
+            print("На нолб делить нельзя")
+            return
+        #Получаем значения X  в указанном диапазоне с шагом
+        x = np.arange(minX, maxX + 1, 1)
+        y = eval(expression)
 
-        x = np.arange(-10, 10, 0.05)
-        #y = np.arange(-10, 10, 0.05)
+        fig, ax = plt.subplots(figsize=(6, 6))
 
-        fig, ax = plt.subplots(figsize=(7, 6))
-        ax.plot(x, eval(expression))
-
+        ax.plot(x, y)
         ax.set(title= f"График функции: { expression }")
 
         ax = plt.gca()
 
         #Настройка оси координат
+        #Добавление сетки, ее толщина
+        ax.grid(axis = 'both', linewidth=1)
         ax.spines['left'].set_position('center')
         ax.spines['bottom'].set_position('zero')
         ax.spines['top'].set_visible(False)
         ax.spines['right'].set_visible(False)
 
+        #  Устанавливаем интервал основных делений:
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(2))
+        #  Устанавливаем интервал вспомогательных делений:
+        ax.xaxis.set_minor_locator(ticker.MultipleLocator(1))
+        ax.yaxis.set_minor_locator(ticker.MultipleLocator(1))
         fig.savefig('graph')
 
         self.changeImage()
